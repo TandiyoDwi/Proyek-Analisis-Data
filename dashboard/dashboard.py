b43f43681a4f692498bbbf48c17c731d49d0b28f
TandiyoDwi <tndiodwi@gmail.com>
2025-03-13 04:24:47 +0700

    Revisi

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,39 +1,46 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-import scipy as sp
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 day_df = pd.read_csv("../data/day.csv")
-hour_df = pd.read_csv("../data/hour.csv")
 
-# Dashboard
-st.title("Bike Sharing Dashboard")
+st.title("Diagram Penyewaan Sepeda Tiap Musim dan Tahun")
+
+# Merubah data
+day_df['season'] = day_df['season'].map({
+    1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'
+})
+day_df['yr'] = day_df['yr'].map({
+    0: '2011', 1: '2012',
+})
+
+day_df['season'] = day_df['season'].astype('category')
+day_df['yr'] = day_df['yr'].astype('category')
 
 # Sidebar
-option = st.sidebar.selectbox("Pilih Data", [ "Pinjaman Tiap Musim", "Performa Rental 2 Tahun"])
+option = st.sidebar.selectbox("Pilih Data", ["Pinjaman Tiap Musim", "Performa Rental 2 Tahun"])
 
 if option == "Pinjaman Tiap Musim":
     st.header("Pinjaman Sepeda Tiap Musim")
+    season_usage = day_df.groupby("season", observed=False)[["registered", "casual"]].sum().reset_index()
     
-    season_rentals = day_df.groupby("season")["cnt"].sum()
-    fig, ax = plt.subplots()
-    season_rentals.plot(kind='bar', color=['blue', 'green', 'red', 'orange'], ax=ax)
-    ax.set_xlabel("Musim")
-    ax.set_ylabel("Total Penyewaan")
-    ax.set_title("Total Penyewaan Sepeda Berdasarkan Musim")
-    ax.set_xticklabels(["Spring", "Summer", "Fall", "Winter"], rotation=0)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.bar(season_usage["season"], season_usage["registered"], label="Registered", color="blue")
+    ax.bar(season_usage["season"], season_usage["casual"], label="Casual", color="red", bottom=season_usage["registered"])
+    
+    ax.set_title("Jumlah Penyewaan Sepeda Berdasarkan Musim")
+    ax.legend()
     st.pyplot(fig)
 
 elif option == "Performa Rental 2 Tahun":
     st.header("Performa Rental Sepeda dalam 2 Tahun")
+    rent_year = day_df.groupby("yr", observed=False)[["cnt"]].sum().reset_index()
     
-    year_rentals = day_df.groupby("yr")["cnt"].sum()
-    fig, ax = plt.subplots()
-    year_rentals.plot(kind='bar', color=['blue', 'green'], ax=ax)
-    ax.set_xlabel("Tahun")
-    ax.set_ylabel("Total Penyewaan")
-    ax.set_title("Total Penyewaan Sepeda Tahun 2011 vs 2012")
-    ax.set_xticklabels(["2011", "2012"], rotation=0)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.bar(rent_year["yr"], rent_year["cnt"], color="blue", label="Total Penyewaan")
+    
+    ax.set_title("Jumlah Penyewaan Sepeda Berdasarkan Tahun")
+    ax.legend()
     st.pyplot(fig)
